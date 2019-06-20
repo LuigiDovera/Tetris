9c6e3f22d9280d41d9b7f295c2072207b52c8ef0
Luigi Dovera <luigidovera11@gmail.com>
2019-06-20 15:43:13 -0300

    aa

--- main.lua
+++ main.lua
@@ -14,17 +14,33 @@ blocks = {img ={i=nil, l=nil, j=nil, o=nil, z=nil, s=nil, t=nil}}
 --Bloco do jogador
 	--O objeto guardará a posição atual do bloco 'jogável' do player
 	--A posição se dá pelos indices do bloco na matriz
-	playerB = {mX = nil, mY = nil}
+	player = {mX = 4, mY = 1, shape = nil}
 	
 --timers
 	--timer para a movimentação vertical do blocos
 		fallTimerMax = 1
 		fallTimer = fallTimerMax
-
+	--timer para restringir as ações do jogador, evitando eventuais bugs
+		actionTimerMax = 0.1
+		actionTimer = actionTimerMax
 
 function updateMatriz()
 
+
 end
+
+
+function blockRandomizer()
+	n = math.random(1, 7)
+
+	if n == 1 then return blocks.img.i end
+	if n == 2 then return blocks.img.l end
+	if n == 3 then return blocks.img.j end
+	if n == 4 then return blocks.img.o end
+	if n == 5 then return blocks.img.z end
+	if n == 6 then return blocks.img.s end
+	if n == 7 then return blocks.img.t end
+end	
 
 function love.load(arg)
 	blocks.img.i = love.graphics.newImage("assets/img/i.png")
@@ -35,21 +51,72 @@ function love.load(arg)
 	blocks.img.s = love.graphics.newImage("assets/img/s.png")
 	blocks.img.t = love.graphics.newImage("assets/img/t.png")
 
+	matriz[player.mY][player.mX] = blocks.img.t
+
 end
 
 function love.update(dt)
-	--[[
+	-- update dos timers
 	fallTimer = fallTimer - dt
-	if fallTimer < 0 then
-		matriz[playerB.mY + 1][playerB.mX] = matriz[playerB.mY][playerB.mX]
-		matriz[playerB.mY][playerB.mX] = nil
-		playerB.mY = playerB.mY + 1
-	end
-	]]
+	actionTimer = actionTimer - dt
 
-	if love.keyboard.isDown('a') or love.keyboard.isDown('left') then
-		player.mX = player.mX - 1
+	--'gravidade' sendo aplicada
+	if fallTimer <= 0 then
+		matriz[player.mY + 1][player.mX] = matriz[player.mY][player.mX]
+		matriz[player.mY][player.mX] = nil
+		player.mY = player.mY + 1
+
+		fallTimer = fallTimerMax
 	end
+
+	-- movimentação horizontal
+	if (love.keyboard.isDown('a') or love.keyboard.isDown('left')) 
+		and actionTimer <=0 then
+		player.mX = player.mX - 1
+
+		if player.mX <= 0 then 
+			player.mX = 1 
+		else
+			matriz[player.mY][player.mX] = matriz[player.mY][player.mX + 1]
+			matriz[player.mY][player.mX + 1] = nil
+		end
+		
+		actionTimer = actionTimerMax
+	end
+
+	if (love.keyboard.isDown('d') or love.keyboard.isDown('right')) 
+		and actionTimer <=0 then
+		player.mX = player.mX + 1
+		if player.mX >= 8 then 
+			player.mX = 7
+		else
+			matriz[player.mY][player.mX] = matriz[player.mY][player.mX - 1]
+			matriz[player.mY][player.mX - 1] = nil
+		end
+
+		actionTimer = actionTimerMax
+	end
+
+	if player.mY == 11 or matriz[player.mY + 1][player.mX] ~= nil then
+		player.mX = 4
+		player.mY = 1
+		matriz[player.mY][player.mX] = blockRandomizer()
+	end
+
+	for i=1, 11 do
+		for j=1, 7 do
+			if matriz[i][j] ~= nil then fullLine = true
+			else fullLine = false
+			end
+
+			if not fullLine then break end
+		end
+		if fullLine then
+			e
+	end
+
+
+
 end
 
 function love.draw(dt)
