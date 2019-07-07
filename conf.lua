809f1917f58f677ab8350a4236b8687ffb4dd725
Luigi Dovera <luigidovera11@gmail.com>
2019-07-07 01:05:25 -0300

    MUITA COISA

--- conf.lua
+++ conf.lua
@@ -1,8 +1,8 @@
 function love.conf(t)
 	t.title = "Tetris" -- The title of the window the game is in (string)
 	t.version = "0.9.1"         -- The LÖVE version this game was made for (string)
-	t.window.width = 50*7     --[valor base * largura em blocos]
-	t.window.height = 50*11		--[valor base * altura em blocos]
+	t.window.width = 50*7     --[valor base (blocks.lado) * largura em blocos (matriz.largura)]
+	t.window.height = 50*11		--[valor base (blocks.lado) * altura em blocos (matriz.altura)]
 
 	-- For Windows debugging
 	t.console = true
--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 
-blocks = {img ={i=nil, l=nil, j=nil, o=nil, z=nil, s=nil, t=nil}}
+blocks = {img ={i=nil, l=nil, j=nil, o=nil, z=nil, s=nil, t=nil}, lado = 50, caindo = false}
 
 
 -- Inicializando a matriz principal que armazenará a informação dos blocos
@@ -14,17 +14,212 @@ blocks = {img ={i=nil, l=nil, j=nil, o=nil, z=nil, s=nil, t=nil}}
 --Bloco do jogador
 	--O objeto guardará a posição atual do bloco 'jogável' do player
 	--A posição se dá pelos indices do bloco na matriz
-	player = {mX = math.ceil(matriz.largura/2), mY = 1, shape = nil}
+	player = {mX = math.ceil(matriz.largura/2), mY = 1, shape = nil, orientacao = nil, 
+		caindo = false, adj = {}}
+
+	for i=1, 3 do
+		player.adj[i] = {mX = nil, mY = nil}
+	end
 	
 --timers
 	--timer para a movimentação vertical do blocos
-		fallTimerMax = 1
+		fallTimerMax = 1000000
 		fallTimer = fallTimerMax
 	--timer para restringir as ações do jogador, evitando eventuais bugs
 		actionTimerMax = 0.2
 		actionTimer = actionTimerMax
 
-function updateMatriz()
+function blockRandomizer()
+	player.mY = 1
+	player.mX = math.ceil(matriz.largura/2)
+
+	math.randomseed(os.time())
+	n = math.random(1, 4)
+
+	--if n == 1 then player.shape = 'i' return blocks.img.i end
+	--if n == 2 then player.shape = 'l' return blocks.img.l end
+	if n == 1 then 
+		player.shape = 'j' 
+		player.mY = 2
+		player.orientacao = 'cima'
+
+		player.adj[1].mY, player.adj[1].mX = player.mY+1, player.mX-1
+		player.adj[2].mY, player.adj[2].mX = player.mY+1, player.mX
+		player.adj[3].mY, player.adj[3].mX = player.mY-1, player.mX
+
+		img = blocks.img.j
+	end
+	if n == 2 then 
+		player.shape = 'o'
+		player.orientacao = 'cima'
+
+		player.adj[1].mY, player.adj[1].mX = player.mY, player.mX+1
+		player.adj[2].mY, player.adj[2].mX = player.mY+1, player.mX+1
+		player.adj[3].mY, player.adj[3].mX = player.mY+1, player.mX
+
+		img = blocks.img.o 
+	end
+	if n == 3 then 
+		player.shape = 'z'
+		player.orientacao = 'cima'
+
+		player.adj[1].mY, player.adj[1].mX = player.mY, player.mX-1
+		player.adj[2].mY, player.adj[2].mX = player.mY+1, player.mX
+		player.adj[3].mY, player.adj[3].mX = player.mY+1, player.mX+1
+
+		img = blocks.img.z
+	end
+	--if n == 6 then player.shape = 's' return blocks.img.s end
+	if n == 4 then 
+		player.shape = 't'
+		player.orientacao = 'cima'
+
+		player.adj[1].mY, player.adj[1].mX = player.mY, player.mX-1
+		player.adj[2].mY, player.adj[2].mX = player.mY+1, player.mX
+		player.adj[3].mY, player.adj[3].mX = player.mY, player.mX+1 
+
+		img = blocks.img.t
+	end
+
+	matriz[player.mY][player.mX] = img
+	for i=1, 3 do
+			matriz[player.adj[i].mY][player.adj[i].mX] = img
+	end
+end	
+
+
+function contarColisoes(direcao, matriz)
+	colisao = 0
+	if direcao == 'esquerda' then
+		if matriz[player.mY][player.mX - 1] ~= nil then
+			colisao = colisao + 1
+		end
+		for i=1, 3 do
+			if matriz[player.adj[i].mY][player.adj[i].mX - 1] ~= nil then
+				colisao = colisao + 1
+			end
+		end
+	elseif direcao == 'direita' then
+		if matriz[player.mY][player.mX + 1] ~= nil then
+			colisao = colisao + 1
+		end
+		for i=1, 3 do 
+			if matriz[player.adj[i].mY][player.adj[i].mX + 1] ~= nil then
+				colisao = colisao + 1
+			end
+		end
+	end
+	return colisao
+end
+
+function corpoIsolado()
+	matrizTeste = {}
+	for i=1, matriz.altura do
+		matrizTeste[i] = {}
+		for j=1, matriz.largura do
+			matrizTeste[i][j] = nil
+		end
+	end
+
+	matrizTeste[player.mY][player.mX] = matriz[player.mY][player.mX]
+	for i=1, 3 do
+		matrizTeste[player.adj[i].mY][player.adj[i].mX] = matriz[player.adj[i].mY][player.adj[i].mX]
+	end
+
+	return matrizTeste
+end
+
+function validaMovimentacao(direcao)
+	if contarColisoes(direcao, matriz) > contarColisoes(direcao,corpoIsolado()) then
+		return false
+	else
+		return true
+	end
+end
+
+function cruzandoFronteiras(direcao)
+	if direcao == 'esquerda' then
+		if player.mX <= 1 then
+			return true
+		end
+		for i=1, 3 do
+			if player.adj[i].mX <= 1 then
+				return true
+			end
+		end
+	elseif direcao == 'direita' then
+		if player.mX >= matriz.largura then
+			return true
+		end
+		for i=1, 3 do
+			if player.adj[i].mX >= matriz.largura then
+				return true
+			end
+		end
+	end
+	return false
+end
+
+function playerMovement(direcao)
+	if direcao == 'esquerda' and not cruzandoFronteiras(direcao) then
+		img = matriz[player.mY][player.mX]
+
+		matriz[player.mY][player.mX] = nil
+		for i=1, 3 do
+			matriz[player.adj[i].mY][player.adj[i].mX] = nil
+		end
+
+		player.mX = player.mX - 1
+		for i=1, 3 do
+			player.adj[i].mX = player.adj[i].mX - 1 
+		end
+
+		matriz[player.mY][player.mX] = img
+		for i=1, 3 do
+			matriz[player.adj[i].mY][player.adj[i].mX] = img
+		end
+
+ 	elseif direcao == 'direita' and not cruzandoFronteiras(direcao) then
+ 		img = matriz[player.mY][player.mX]
+
+		matriz[player.mY][player.mX] = nil
+		for i=1, 3 do
+			matriz[player.adj[i].mY][player.adj[i].mX] = nil
+		end
+
+		player.mX = player.mX + 1
+		for i=1, 3 do
+			player.adj[i].mX = player.adj[i].mX + 1 
+		end
+
+		matriz[player.mY][player.mX] = img
+		for i=1, 3 do
+			matriz[player.adj[i].mY][player.adj[i].mX] = img
+		end
+
+	elseif direcao == 'baixo' then
+		if shape == 'j' then
+
+		elseif shape == 'o' then
+
+		elseif shape == 'z' then
+	
+		elseif shape == 't' then
+	
+		end
+	elseif direcao == 'cima' then
+		if shape == 'j' then
+
+		elseif shape == 'o' then
+
+		elseif shape == 'z' then
+	
+		elseif shape == 't' then
+	
+		end
+	else
+		print('direção inválida')
+	end 
 end
 
 function topBlock()
@@ -38,18 +233,6 @@ function topBlock()
 	return matriz.altura
 end
 
-function blockRandomizer()
-	n = math.random(1, 7)
-
-	if n == 1 then return blocks.img.i end
-	if n == 2 then return blocks.img.l end
-	if n == 3 then return blocks.img.j end
-	if n == 4 then return blocks.img.o end
-	if n == 5 then return blocks.img.z end
-	if n == 6 then return blocks.img.s end
-	if n == 7 then return blocks.img.t end
-end	
-
 function love.load(arg)
 	blocks.img.i = love.graphics.newImage("assets/img/i.png")
 	blocks.img.l = love.graphics.newImage("assets/img/l.png")	
@@ -60,7 +243,7 @@ function love.load(arg)
 	blocks.img.t = love.graphics.newImage("assets/img/t.png")
 
 	-- Inicializando o jogador
-	matriz[player.mY][player.mX] = blockRandomizer()
+	blockRandomizer()
 
 end
 
@@ -70,60 +253,50 @@ function love.update(dt)
 	actionTimer = actionTimer - (1*dt)
 
 	--'gravidade' sendo aplicada
-	if fallTimer <= 0 then
+	--[[if fallTimer <= 0 then
 		matriz[player.mY + 1][player.mX] = matriz[player.mY][player.mX]
 		matriz[player.mY][player.mX] = nil
 		player.mY = player.mY + 1
 
 		fallTimer = fallTimerMax
-	end
+	end]]
 
-	-- Controles do usuário
-		-- movimentação horizontal
-	if (love.keyboard.isDown('a') or love.keyboard.isDown('left')) 
-		and actionTimer <=0 then
+	if not player.caindo then
+		-- Controles do usuário
+			-- movimentação horizontal
+		if (love.keyboard.isDown('a') or love.keyboard.isDown('left')) 
+			and actionTimer <=0 then
 
-		player.mX = player.mX - 1
-
-		if player.mX <= 0 then 
-			player.mX = 1 
-		else
-			matriz[player.mY][player.mX] = matriz[player.mY][player.mX + 1]
-			matriz[player.mY][player.mX + 1] = nil
-		end
-		
-		actionTimer = actionTimerMax
-	end
-
-	if (love.keyboard.isDown('d') or love.keyboard.isDown('right')) 
-		and actionTimer <=0 then
-
-		player.mX = player.mX + 1
-		if player.mX >= matriz.largura + 1 then 
-			player.mX = matriz.largura
-		else
-			matriz[player.mY][player.mX] = matriz[player.mY][player.mX - 1]
-			matriz[player.mY][player.mX - 1] = nil
+			playerMovement('esquerda')
+			
+			actionTimer = actionTimerMax
 		end
 
-		actionTimer = actionTimerMax
-	end
-		-- fim da movimentação horizontal
+		if (love.keyboard.isDown('d') or love.keyboard.isDown('right')) 
+			and actionTimer <=0 then
 
-		-- movimentação vertical
-	if love.keyboard.isDown(' ') and actionTimer <=0 then
+			playerMovement('direita')
 
+			actionTimer = actionTimerMax
+		end
+			-- fim da movimentação horizontal
+
+			-- movimentação vertical
+		if love.keyboard.isDown(' ') and actionTimer <=0 then
+
+			player.caindo = true
+
+		end
+			-- fim da movimentação vertical
+		-- fim dos controles do usuário
+	else
 		topo = topBlock()
-		
-		matriz[player.mY][player.mX],matriz[topo][player.mX]
-			= matriz[topo][player.mX],matriz[player.mY][player.mX]
-
-		player.mY = topo
-
-		actionTimer = actionTimerMax
-	end 
-		-- fim da movimentação vertical
-	-- fim dos controles do usuário
+		fallTimerMax = 0.05
+		if player.mY == topo then
+			player.caindo = false
+			fallTimerMax = 1
+		end
+	end
 
 	-- Contabilizando pontos e destruindo linhas
 	for i=1, matriz.altura do
@@ -143,26 +316,25 @@ function love.update(dt)
 			for j=1, matriz.largura do
 				matriz[i][j] = nil
 			end 
-			caindo = true 
+			blocks.caindo = true 
 		end
 	end
 
-	if caindo then
+	if blocks.caindo then
 		for i=matriz.altura, 2, -1 do
 			for j=1, matriz.largura do
 				matriz[i][j], matriz[i-1][j] = matriz[i-1][j], matriz[i][j]
 			end
 		end 
-		caindo = false
+		blocks.caindo = false
 	end
 	-- fim da atualização de linhas
 	
 	-- Se o jogador estiver no fundo da matriz ou se a posição abaixo do jogador estiver ocupada
 	-- então é gerado um novo bloco jogável e a posição do jogador é relocada para tal
-	if player.mY == matriz.altura or matriz[player.mY + 1][player.mX] ~= nil then
-		player.mY = 1
-		matriz[player.mY][player.mX] = blockRandomizer()
-	end
+	--[[if player.mY == matriz.altura or matriz[player.mY + 1][player.mX] ~= nil then
+		blockRandomizer()
+	end]]
 
 
 end
@@ -171,7 +343,7 @@ function love.draw(dt)
 	for i=1, matriz.altura do
 		for j=1, matriz.largura do
 			if matriz[i][j] ~= nil then
-				love.graphics.draw(matriz[i][j], 50*(j-1), 50*(i-1))
+				love.graphics.draw(matriz[i][j], blocks.lado*(j-1), blocks.lado*(i-1))
 			end
 		end
 	end
